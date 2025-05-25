d3f89e1574392abac496c7b8258e6d0d9ef19c5f
Robin Wessel <robinwessel1@gmail.com>
2025-05-25 21:17:37 +0200

    Update Fix

--- PluginUpdater/Properties/AssemblyInfo.cs
+++ PluginUpdater/Properties/AssemblyInfo.cs
@@ -1,11 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Plugin Updater")]
+[assembly: AssemblyTitle("PluginUpdater")]
 [assembly: AssemblyDescription("Updates all Plugins")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("T3rr0rS0ck3")]
